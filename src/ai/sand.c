bb9c9e7167e75b96e89f43b9667337a21f76f557
Andwn <cirno@tfwno.gf>
2016-08-04 16:10:50 -0400

    Crow, Skullhead, Bar Mimigas

--- src/ai/sand.c
+++ src/ai/sand.c
@@ -259,7 +259,7 @@ void ai_polishBaby_onUpdate(Entity *e) {
 }
 
 void ai_sandcroc_onUpdate(Entity *e) {
-	int pbottom, crocbottom;
+	//int pbottom, crocbottom;
 	switch(e->state) {
 		case 0:
 			e->state = 1;
@@ -381,4 +381,249 @@ void ai_armadillo_onUpdate(Entity *e) {
 	
 	e->y_speed += 0x40;
 	LIMIT_Y(0x5ff);
+}
+
+void ai_crow_onUpdate(Entity *e) {
+	switch(e->state) {
+		case 0:
+		{
+			u16 angle = random() % 1024;
+			e->x_speed = sintab32[angle] >> 1;
+			e->y_speed = sintab32[(angle + 256) % 1024] >> 1;
+			e->x_mark = e->x + (e->x_speed * 8);
+			e->y_mark = e->y + (e->y_speed * 8);
+			e->state = 1;
+		}
+		case 1:
+		case 101:
+			if (e->x > e->x_mark) e->x_speed -= 16;
+			else if (e->x < e->x_mark) e->x_speed += 16;
+			
+			if (e->y > e->y_mark) e->y_speed -= 16;
+			else if (e->y < e->y_mark) e->y_speed += 16;
+			
+			FACE_PLAYER(e);
+			SPR_SAFEHFLIP(e->sprite, e->direction);
+			LIMIT_X(0x200);
+			LIMIT_Y(0x200);
+			
+			if (e->damage_time) {
+				e->state++;		// state 2/102
+				e->state_time = 0;
+				e->y_speed = 0;
+			}
+		break;
+		
+		case 2:
+		case 102:
+			FACE_PLAYER(e);
+			
+			if (e->damage_time) {
+				// fall while hurt
+				e->y_speed += 0x20;
+				e->x_speed = 0;
+			} else {
+				// move towards player
+				if(e->x < player.x) e->x_speed += 0x10;
+				else e->x_speed -= 0x10;
+				if(e->y < player.y) e->y_speed += 0x10;
+				else e->y_speed -= 0x10;
+			}
+			// bounce off walls
+			if (e->x_speed < 0 && collide_stage_leftwall(e)) e->x_speed = 0x200;
+			if (e->x_speed > 0 && collide_stage_rightwall(e)) e->x_speed = -0x200;
+			
+			if (e->y_speed < 0 && collide_stage_ceiling(e)) e->y_speed = 0x200;
+			if (e->y_speed > 0 && collide_stage_floor(e)) e->y_speed = -0x200;
+			
+			LIMIT_X(0x5ff);
+			LIMIT_Y(0x5ff);
+		break;
+	}
+}
+
+void ai_skullhead_onUpdate(Entity *e) {
+	switch(e->state) {
+		case 0:
+			e->state = 1;
+			e->state_time = random() % 5;
+		case 1:
+			if(++e->state_time > 8) {
+				e->y_speed = -0x350;
+				e->state = 2;
+				SPR_SAFEANIM(e->sprite, 1);
+				MOVE_X(0x100);
+			}
+			else break;
+		case 2:
+			if (collide_stage_floor(e)) {
+				e->x_speed = 0;
+				e->state = 1;
+				e->state_time = 0;
+				SPR_SAFEANIM(e->sprite, 0);
+			}
+			
+			if(e->y_speed < 0) {
+				collide_stage_ceiling(e);
+			} else {
+				SPR_SAFEANIM(e->sprite, 0);
+			}
+		break;
+	}
+	
+	if(e->x_speed) {
+		if (collide_stage_leftwall(e)) { e->direction = 1; e->x_speed = 0x100; }
+		if (collide_stage_rightwall(e)) { e->direction = 0; e->x_speed = -0x100; }
+	}
+	
+	e->y_speed += 0x40;
+	LIMIT_Y(0x5ff);
+}
+
+void ai_skelShot_onUpdate(Entity *e) {
+	// bounce off walls
+	if((e->x_speed < 0 && collide_stage_leftwall(e)) || 
+		(e->x_speed > 0 && collide_stage_rightwall(e))) {
+		e->x_speed = -e->x_speed;
+		e->state_time += 5;
+	}
+	
+	// bounce off ceiling
+	if(e->y_speed < 0 && collide_stage_ceiling(e)) {
+		e->y_speed = -e->y_speed;
+		e->state_time += 5;
+	}
+	
+	// if hit floor bounce along it...
+	if (collide_stage_floor(e)) {
+		e->y_speed = -0x180;
+		e->state = 1;	// begin falling
+		e->state_time += 4;
+	}
+	
+	if(e->state == 1) {
+		e->y_speed += 0x10;
+		LIMIT_Y(0x5ff);
+	}
+	
+	if(e->state_time >= 10) {
+		e->state = STATE_DELETE;
+	}
+}
+
+// curly's mimiga's
+void ai_curlys_mimigas(Entity *e) {
+	switch(e->state) {
+		case 0:		// init/set initial anim state
+			if(e->type == OBJ_MIMIGAC1) {
+				e->state = 100;
+			} else { // OBJ_MIMIGAC2 & OBJ_MIMIGA_ENEMY
+				e->state = 2;			// stand and blink
+				if(e->type == OBJ_MIMIGAC2) {
+					if(e->direction) e->state = 110; // sleeping
+				}
+			}
+			ai_curlys_mimigas(e);		// re-process again with correct state
+		break;
+		
+		case 2:		// init stand and blink
+			e->state = 3;
+		case 3:		// stand and blink
+			//o->frame = 0;
+			//randblink(o, 1, 8);
+		break;
+		
+		// sitting mimiga (when facing right)
+		// facing away mimiga (when facing left)
+		case 100:
+			//o->frame = 4;
+		break;
+		
+		case 110:	// sleeping facing left mimiga
+			//o->frame = 7;
+			//ai_zzzz_spawner(o);
+		break;
+		
+		/// ******************** Fighting Mimiga Code ********************
+		case 10:
+			e->eflags |= NPC_SHOOTABLE;
+			
+			e->health = 1000;
+			e->state = 11;
+			
+			e->state_time = random() % 50;
+			SPR_SAFEANIM(e->sprite, 0);
+		//fall thru to state 11
+		case 11:
+			if(e->state_time) e->state_time--;
+			else e->state = 13;
+		break;
+		
+		case 13:
+			e->state = 14;
+			e->state_time = random() % 50;
+			FACE_PLAYER(e);
+			SPR_SAFEANIM(e->sprite, 1);
+			// fall thru
+		case 14:
+			if(e->direction) e->x_speed += 0x40;
+			else e->x_speed -= 0x40;
+			
+			if(e->state_time) {
+				e->state_time--;
+			} else {	
+				// enter hop state
+				e->state = 15;
+				//e->frame = 2;
+				e->y_speed = -0x200;
+				e->attack = 2;
+			}
+		break;
+		
+		case 15:	// hopping
+			if((e->grounded = collide_stage_floor(e))) {	
+				// landed
+				e->x_speed = 0;
+				e->state = 10;
+				e->attack = 0;
+			}
+		break;
+		
+		case 20:	// aiiie! got shot!!
+			if((e->grounded = collide_stage_floor(e))) {
+				e->x_speed = 0;
+				e->state = 21;
+				//if (o->frame==6) o->frame = 7;
+				//			else o->frame = 8;
+				
+				e->state_time = 300 + (random() % 100);
+			}
+		break;
+		
+		case 21:	// lying on ground knocked out
+			if (e->state_time) {
+				e->state_time--;
+				break;
+			}
+			e->eflags |= NPC_SHOOTABLE;
+			e->health = 1000;
+			e->state = 11;
+			e->state_time = random() % 50;
+			SPR_SAFEANIM(e->sprite, 0);
+		break;
+	}
+	
+	if (e->state > 10 && e->state < 20 && e->health != 1000)
+	{	// got shot by player
+		e->state = 20;
+		e->y_speed = -0x200;
+		//e->frame = random(5, 6);
+		
+		e->attack = 0;
+		e->eflags &= ~NPC_SHOOTABLE;
+	}
+	
+	e->y_speed += 0x20;
+	LIMIT_Y(0x5ff);
+	LIMIT_X(0x1ff);
 }
