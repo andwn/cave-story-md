dd5e906820ec654b6182aedc8f44afb0b32c4825
Grind <cirno@tfwno.gf>
2017-06-28 16:19:11 -0400

    It almost works I swear

--- src/kanji.c
+++ src/kanji.c
@@ -16,8 +16,8 @@
 // TILE_NUMBERINDEX (16 tiles)
 
 const uint8_t tileorder[] = {
-	15, 31, 14, 30, 13, 29, 12, 28, 11, 27, 10, 26, 9, 25, 8, 24,
-	7, 23, 6, 22, 5, 21, 4, 20, 3, 19, 2, 18, 1, 17, 0, 16
+	0,8,1,9,2,10,3,11,4,12,5,13,6,14,7,15,
+	16,24,17,25,18,26,19,27,20,28,21,29,22,30,23,31
 };
 
 void kanji_draw(VDPPlan plan, uint16_t vramIndex, uint16_t chr, uint16_t x, uint16_t y, uint16_t backCol) {
@@ -25,25 +25,25 @@ void kanji_draw(VDPPlan plan, uint16_t vramIndex, uint16_t chr, uint16_t x, uint
 	const uint8_t *bmp;
 	if(chr > 0xFF) {
 		chr -= 0x100;
-		bmp = BMP_Kanji + sizeof(BMP_Kanji) - (chr * 32);
+		bmp = BMP_Kanji + 65154 - (chr * 32);
 	} else {
 		chr -= 0x20;
-		bmp = BMP_Ascii + sizeof(BMP_Ascii) - (chr * 32);
+		bmp = BMP_Ascii + 6274 - (chr * 32);
 	}
 	uint32_t tiles[32] = {};
-	for(uint16_t i = 0; i < 32; i++) {
-		uint32_t row = *--bmp;
-		for(uint16_t col = 0; col < 8; col++) {
-			uint32_t color = (row & (1 << col)) ? 15 : backCol;
-			tiles[tileorder[i]] = color << (col*4);
+	for(uint8_t i = 0; i < 32; i++) {
+		uint8_t row = *(--bmp);
+		for(uint8_t col = 0; col < 8; col++) {
+			uint32_t color = (row & (1<<col)) ? 15 : backCol;
+			tiles[tileorder[i]] |= color << (col*4);
 		}
 	}
 	if(vramIndex >= 0xB000 >> 5 && vramIndex < 0xC000 >> 5) {
 		// Between gaps in the window mapping, can't load in sequence
-		VDP_loadTileData(tiles, vramIndex,   1, TRUE);
-		VDP_loadTileData(tiles, vramIndex+4, 1, TRUE);
-		VDP_loadTileData(tiles, vramIndex+8, 1, TRUE);
-		VDP_loadTileData(tiles, vramIndex+12,1, TRUE);
+		VDP_loadTileData(&tiles[0],  vramIndex,   1, TRUE);
+		VDP_loadTileData(&tiles[8],  vramIndex+4, 1, TRUE);
+		VDP_loadTileData(&tiles[16], vramIndex+8, 1, TRUE);
+		VDP_loadTileData(&tiles[24], vramIndex+12,1, TRUE);
 		VDP_setTileMapXY(plan, TILE_ATTR_FULL(PAL0,1,0,0,vramIndex),   x,  y);
 		VDP_setTileMapXY(plan, TILE_ATTR_FULL(PAL0,1,0,0,vramIndex+4), x+1,y);
 		VDP_setTileMapXY(plan, TILE_ATTR_FULL(PAL0,1,0,0,vramIndex+8), x,  y+1);
