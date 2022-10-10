aba7b39209454d123e5101302501f4216c80d04a
Andrew DeRosier <andy@skychase.zone>
2022-10-11 03:57:04 +0900

    Shouldn't forget this...

--- src/vdp.c
+++ src/vdp.c
@@ -261,7 +261,12 @@ void vdp_fade(const uint16_t *src, const uint16_t *dst, uint16_t speed, uint8_t 
     if(!async) {
         while(vdp_fade_step_calc()) {
 			vdp_vsync();
+
+            disable_ints;
+            z80_request();
 			vdp_fade_step_dma();
+            z80_release();
+            enable_ints;
 		}
     }
 }
