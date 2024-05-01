73c556f368a3cde2830fe89ca209696a2114bd33
Andrew DeRosier <andy@skychase.zone>
2024-05-02 01:50:25 +0900

    What the hell happened to dma.h

--- src/md/dma.h
+++ src/md/dma.h
@@ -3,4 +3,18 @@
 
 #include "types.h"
 
+#define DmaVRAM    0x40000080LU
+#define DmaCRAM    0xC0000080LU
+#define DmaVSRAM   0x40000090LU
+
+#define dma_now(cmd, from, to, len, inc) {	\
+	dma_queue(cmd, from, to, len, inc);		\
+	dma_pop();								\
+}
+
+extern void dma_queue(uint32_t cmd, uint32_t from, uint16_t to, uint16_t len, uint16_t inc);
+extern void dma_pop();
+extern void dma_flush();
+extern void dma_clear();
+
 #endif //MD_DMA_H
